93c97f41249cf36c494844735c6bb663533791af
Mizzou Neuro <adin.aberbach@gmail.com>
2021-03-05 14:12:13 -0600

    EPSP tuning for new synapses

--- EPSP_Tuning/br.sh
+++ EPSP_Tuning/br.sh
@@ -1,8 +1,10 @@
 python build_network.py 500
+#python build_all_segs.py
+#python run_network.py 0.8 0
 #python run_network.py 0.4 0
 #python run_network.py 0.45 0.4
 #python run_network.py 0.77 0.918
-python run_network.py 1 1.3
+python run_network.py 0.95 1.3 #WHAT I WAS GONNA USE
 #python run_network.py -0.702 0.8752
 #python run_network.py 0.65 0.445
 #python run_network.py 0.69 0.44
