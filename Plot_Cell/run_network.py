59aa6ee0489661f479343249980d342e3853a12f
Mizzou Neuro <adin.aberbach@gmail.com>
2021-03-24 15:13:05 -0500

    Looking at apical diameters

--- Plot_Cell/run_network.py
+++ Plot_Cell/run_network.py
@@ -30,8 +30,8 @@ sim = bionet.BioSimulator.from_config(conf, network=graph)
 
 
 dic = {}
-dic["high"] = {"dist": [], "Ih": [], "LVAst":[], "HVA": []}
-dic["low"] = {"dist": [], "Ih": [], "LVAst":[], "HVA": []}
+dic["high"] = {"dist": [], "Ih": [], "LVAst":[], "HVA": [], "diam": []}
+dic["low"] = {"dist": [], "Ih": [], "LVAst":[], "HVA": [], "diam": []}
 
 #segs = list(graph.get_local_cells().values())[0]._morph.seg_prop
 #import pdb; pdb.set_trace()
@@ -40,12 +40,15 @@ hobj = cell.hobj
 import matplotlib.pyplot as plt
 ps = h.PlotShape(False)  # False tells h.PlotShape not to use NEURON's gui
 h.distance(sec=cell.hobj.soma[0])
+diams1 = []
+diams2 = []
 for sec in hobj.all:
     fullsecname = sec.name()
     sec_type = fullsecname.split(".")[1][:4]
     sec_id = int(fullsecname.split("[")[-1].split("]")[0])
     #sec.v = np.random.random(1)[0]*1
     #print(sec.v)
+
     for seg in sec:
         #import pdb; pdb.set_trace()
         dist = h.distance(seg)
@@ -59,31 +62,52 @@ for sec in hobj.all:
             if dist < 500: #magenta
                 seg.v = 20
             if dist >= 500:
-                if sec_id >= 60: #Red
-                    # dic["high"]["dist"].append(dist)
+                if sec_id >= 60: #Red #WAY more attenuation
+                    #import pdb; pdb.set_trace()
+                    diams1.append(seg.diam)
+                    dic["high"]["dist"].append(dist)
+                    dic["high"]["diam"].append(seg.diam)
                     # dic["high"]["Ih"].append(seg.gIhbar_Ih)
                     # dic["high"]["LVAst"].append(seg.gCa_LVAstbar_Ca_LVAst)
                     # dic["high"]["HVA"].append(seg.gCa_HVAbar_Ca_HVA)
                     seg.v = 40
                 else: #teel
-                    # dic["low"]["dist"].append(dist)
+                    diams2.append(seg.diam)
+                    dic["low"]["dist"].append(dist)
+                    dic["low"]["diam"].append(seg.diam)
                     # dic["low"]["Ih"].append(seg.gIhbar_Ih)
                     # dic["low"]["LVAst"].append(seg.gCa_LVAstbar_Ca_LVAst)
                     # dic["low"]["HVA"].append(seg.gCa_HVAbar_Ca_HVA)
-                    seg.v = -10
+                    seg.v = -20
 
-# for key in dic["low"].keys():
-#     plt.figure()
-#     plt.scatter(dic["low"]["dist"], dic["low"][key])
-#     plt.title("low" + str(key))
+print("High ID/Attenuation Mean Diam:", np.mean(dic["high"]["diam"]))
+print("Low ID/Attenuation Mean Diam:", np.mean(dic["low"]["diam"]))
 
-#     plt.figure()
-#     plt.scatter(dic["high"]["dist"], dic["high"][key])
-#     plt.title("high" + str(key))
+#import pdb; pdb.set_trace()
+for key in dic["low"].keys():
+    if len(dic["low"][key]) != 0:
+        plt.figure()
+        plt.scatter(dic["low"]["dist"], dic["low"][key])
+        plt.title("Low ID/Attenuation " + str(key))
 
-# plt.show()
+        plt.xlim([700, 1300])
+        plt.ylim([0, 2.5])
 
-import pdb; pdb.set_trace()
+        plt.xlabel("Distance (um)")
+        plt.ylabel("Diameter")
+
+        plt.figure()
+        plt.scatter(dic["high"]["dist"], dic["high"][key])
+        plt.title("High ID/Attenuation " + str(key))
+        plt.xlim([700, 1300])
+        plt.ylim([0, 2.5])
+
+        plt.xlabel("Distance (um)")
+        plt.ylabel("Diameter")
+
+plt.show()
+
+#import pdb; pdb.set_trace()
 cmap = cm.hsv
 #import pdb; pdb.set_trace()
 
