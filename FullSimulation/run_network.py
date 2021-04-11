6aa0e477e7b9f8c425132cbfa0382a4d229610fa
Mizzou Neuro <adin.aberbach@gmail.com>
2021-04-11 17:48:43 -0500

    Added Connections.csv tracking

--- FullSimulation/run_network.py
+++ FullSimulation/run_network.py
@@ -2,6 +2,7 @@ from bmtk.simulator import bionet
 import numpy as np
 from neuron import h
 import synapses 
+import pandas as pd
 
 synapses.load()
 syn = synapses.syn_params_dicts()
@@ -23,6 +24,45 @@ conf.build_env()
 graph = bionet.BioNetwork.from_config(conf)
 sim = bionet.BioSimulator.from_config(conf, network=graph)
 
+cells = graph.get_local_cells()
+cell = cells[list(cells.keys())[0]]
+
+h.distance(sec=cell.hobj.soma[0])
+
+sec_types = []
+weights = []
+dists = []
+node_ids = []
+names = []
+source_pops = []
+
+for c in cell.connections():
+    con = c._connector
+    source = c.source_node
+    syn = con.syn()
+    seg = con.postseg()
+    fullsecname = seg.sec.name()
+
+    source_pops.append(source._population)
+    node_ids.append(source._node_id)
+
+    weights.append(float(syn.initW))
+    names.append(fullsecname)
+    sec_types.append(fullsecname.split(".")[1][:4])
+    dists.append(float(h.distance(seg)))
+
+df = pd.DataFrame()
+df["Node ID"] = node_ids
+df["Distance"] = dists
+df["Conductance"] = weights
+df["Type"] = sec_types
+df["Name"] = names
+df["Source Population"] = source_pops
+#df["Node ID"] = node_ids
+#import pdb; pdb.set_trace()
+# df.to_csv("EPSPs.csv", index=False)
+# import pdb; pdb.set_trace()
+df.to_csv("Connections.csv", index=False)
 # def integerize(val):
 #     return int(val)
 
@@ -31,6 +71,6 @@ sim = bionet.BioSimulator.from_config(conf, network=graph)
 # v_mod._transforms["v"] = integerize
 # v_mod._variables = []
 
-#import pdb; pdb.set_trace()
+import pdb; pdb.set_trace()
 sim.run()
 # pc.barrier()
