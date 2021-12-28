11198e1e911b37eb053349008012aa7077506c20
latimerb <latimerb@missouri.edu>
2021-12-28 14:47:31 -0600

    added ability to specify sub variables to record

--- L5NeuronSimulation/FullSimulation/bmtk_modifications.py
+++ L5NeuronSimulation/FullSimulation/bmtk_modifications.py
@@ -12,7 +12,10 @@ def step(self, sim, tstep):
             pop_id = self._gid_map.get_pool_id(gid)
             cell = sim.net.get_cell_gid(gid)
             for var_name in self._variables:
-                seg_vals = [getattr(seg, var_name, None) for seg in cell.get_segments()]
+                if '.' in var_name:
+                    seg_vals = [getattr(getattr(seg, var_name.split('.')[0], None), var_name.split('.')[1], None) for seg in cell.get_segments()]
+                else:
+                    seg_vals = [getattr(seg, var_name, None) for seg in cell.get_segments()]
                 self._var_recorder.record_cell(pop_id.node_id, population=pop_id.population, vals=seg_vals, tstep=tstep)
 
             for var_name, fnc in self._transforms.items():
