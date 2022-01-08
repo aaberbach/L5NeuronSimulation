c030fbf114fce5ae8719ed49b311e04246996e6b
Ben Latimer <latimerb@missouri.edu>
2022-01-08 08:52:38 -0500

    Update bmtk_modifications.py
    Added ability to measure synaptic currents directly

--- L5NeuronSimulation/FullSimulation/bmtk_modifications.py
+++ L5NeuronSimulation/FullSimulation/bmtk_modifications.py
@@ -13,11 +13,17 @@ def step(self, sim, tstep):
             cell = sim.net.get_cell_gid(gid)
             for var_name in self._variables:
                 if '.' in var_name:
-                    seg_vals = [getattr(getattr(seg, var_name.split('.')[0], None), var_name.split('.')[1], None) for seg in cell.get_segments()]
+                    seg_vals = [getattr(getattr(seg, var_name.split('.')[0], None),var_name.split('.')[1],None) for seg in cell.get_segments()]
+                elif var_name == 'igaba':
+                    seg_vals = [sum([getattr(syn,'igaba',0) for syn in seg.point_processes()]) for seg in cell.get_segments()]
+                elif var_name == 'iampa':
+                    seg_vals = [sum([getattr(syn,'iampa',0) for syn in seg.point_processes()]) for seg in cell.get_segments()]
+                elif var_name == 'inmda':
+                    seg_vals = [sum([getattr(syn,'inmda',0) for syn in seg.point_processes()]) for seg in cell.get_segments()]
                 else:
-                    seg_vals = [getattr(seg, var_name, None) for seg in cell.get_segments()]
+                    seg_vals = [getattr(seg, var_name[0], None) for seg in cell.get_segments()]
                 self._var_recorder.record_cell(pop_id.node_id, population=pop_id.population, vals=seg_vals, tstep=tstep)
-
+                
             for var_name, fnc in self._transforms.items():
                 seg_vals = [fnc(getattr(seg, var_name)) for seg in cell.get_segments()]
                 # self._var_recorder.record_cell(gid, var_name, seg_vals, tstep)
